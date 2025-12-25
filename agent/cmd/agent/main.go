79104a7ee4a120e853e1c38529e2d7233186b5c9
tarkanic909 <tarkanic909@gmail.com>
2025-12-25 16:46:16 +0100

    add .env .gitignore use env vars in docker

--- agent/cmd/agent/main.go
+++ agent/cmd/agent/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/tarkanic909/KloudOpsMonitor/agent/internal/config"
+)
+
 func main() {
-	println("Main func")
+	cfg := config.Load()
+	addr := ":" + cfg.Port
+
+	log.Printf("Starting agent %s in %s mode, connecting to backend %s", cfg.AgentID, cfg.Env, cfg.APIURL)
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	http.HandleFunc("/ping-backend", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get(cfg.APIURL + "/health")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("backend unreachable"))
+		}
+
+		defer resp.Body.Close()
+		w.WriteHeader(resp.StatusCode)
+		w.Write([]byte("backend reachable"))
+	})
+
+	log.Printf("Agent running on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
