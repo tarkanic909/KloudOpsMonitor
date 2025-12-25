970cb3b9f70cccc0ab4aa6a9d44a5ba2f5d1ae0a
tarkanic909 <tarkanic909@gmail.com>
2025-12-25 22:31:26 +0100

    impove code readibility

--- agent/cmd/agent/main.go
+++ agent/cmd/agent/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,10 +11,10 @@ import (
 	"time"
 
 	"github.com/tarkanic909/KloudOpsMonitor/agent/internal/config"
+	"github.com/tarkanic909/KloudOpsMonitor/agent/internal/logging"
 )
 
-func main() {
-	cfg := config.Load()
+func createServer(cfg config.Config) *http.Server {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -26,28 +27,72 @@ func main() {
 		Handler: mux,
 	}
 
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	return server
+}
 
+func runServer(ctx context.Context, server *http.Server, shutdownTimeout time.Duration, logger *slog.Logger) error {
+	serverErr := make(chan error, 1)
+
+	// Run HTTP server in separate goroutine
 	go func() {
-		log.Printf("Starting agent %s in %s mode, connecting to backend %s", cfg.AgentID, cfg.Env, cfg.APIURL)
 		err := server.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen error: %v", err)
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
 		}
+		close(serverErr)
 	}()
 
-	<-stop
-	log.Println("Shutdown signal received")
+	stopSig := make(chan os.Signal, 1)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	// Use os.Interrupt is platform idependent, SIGTERM for kubernetes or docker
+	signal.Notify(stopSig, os.Interrupt, syscall.SIGTERM)
 
-	err := server.Shutdown(ctx)
-	if err != nil {
-		log.Printf("Graceful shutdown failed: %v", err)
+	// Check channels for errors and signals
+	select {
+	case err := <-serverErr:
+		return err
+	case <-stopSig:
+		logger.Info("Shutdown signal received")
+	case <-ctx.Done():
+		logger.Info("Context cancelled")
 	}
 
-	log.Println("server shutdown cleanly")
+	// Gracefull shutdown with timeout
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	// Stop receiving new requests and wait to finish existing
+	err := server.Shutdown(shutdownCtx)
+	if err != nil {
+		closeErr := server.Close()
+		if closeErr != nil {
+			return errors.Join(err, closeErr)
+		}
+		return err
+	}
+
+	logger.Info("Server shutdown gracefully")
+	return nil
+}
+
+func main() {
+	// Read configuration
+	cfg := config.Load()
+	// Initialize structured logger
+	logger := logging.New(cfg.Env)
+
+	// Create HTTP router(ServeMux)
+	server := createServer(cfg)
+
+	logger.Info("agent started",
+		"agent_id", cfg.AgentID,
+		"env", cfg.Env,
+		"port", cfg.Port,
+	)
+
+	err := runServer(context.Background(), server, 10*time.Second, logger)
+	if err != nil {
+		logger.Error("Agent error", "error", err)
+	}
 
 }
